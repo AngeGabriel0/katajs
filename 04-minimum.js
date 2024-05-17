9b994efe86dff303412afba13727081fc943a5b7
Gabriel <gabriel.luthun@gmail.com>
2024-05-17 16:31:18 +0200

    feat:add_function_min_exercise

---
+++ 04-minimum.js
@@ -0,0 +1,8 @@
+'use strict'
+function min(a, b) {
+  //Math.min renvoie le plus petit nombre d'une liste d'arguments
+  Math.min(a, b);
+  return Math.min(a, b);
+}
+
+console.log(min(34, 56)); //la console retourne 34, le plus petit nombre
